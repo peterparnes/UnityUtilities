7242243650cc81aafeeb6e75cba2af38177772d3
Peter Parnes <peter@parnes.com>
2017-04-07 14:14:18 +0200

    Added Rotate View

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# UnityUtilities
-Various utilities for Unity
+# Unity Utilities
+Various utilities for Unity. 
 
 # FrameGrabber 
 Grab frames from a scene. 
+
+# RotateView 
+Rotate the camera around a target object. 
+To change direction of the camera rotation, set a negative Turn Speed. 
 
 /Peter Parnes, peter@parnes.com 
---
+++ RotateView/Scripts/RotateView.cs
@@ -0,0 +1,31 @@
+﻿// Peter Parnes, peter@parnes.com, 20170407
+
+using UnityEngine;
+using System.Collections;
+
+public class RotateView : MonoBehaviour {
+
+	public float turnSpeed = 40.0f;
+	public Transform target;
+
+	public float height = 2f;
+	public float distance = 2f;
+	public float lookYOffset = 1.4f;
+
+	private Vector3 offsetX;
+	private Vector3 offsetY;
+	private Vector3 offsetLookY;
+
+	void Start () {
+		offsetX = new Vector3 (0, height, distance);
+		offsetY = new Vector3 (0, 0, distance);
+		offsetLookY = new Vector3 (0, lookYOffset, 0);
+	}
+
+	void LateUpdate() {
+		offsetX = Quaternion.AngleAxis (Time.deltaTime * turnSpeed, Vector3.up) * offsetX;
+		offsetY = Quaternion.AngleAxis (Time.deltaTime * turnSpeed, Vector3.right) * offsetY;
+		transform.position = target.position + offsetX; 
+		transform.LookAt(target.position+offsetLookY);
+	}
+}
